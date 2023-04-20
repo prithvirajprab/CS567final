d7be81a371f92a0d6b7e6b929ff662ced64b3bdb
prithvirajprab <prithvirajprab@gmail.com>
2023-04-20 10:54:00 -0700

    Absolute path and some debugging updates

--- single_pass.py
+++ single_pass.py
@@ -20,15 +20,16 @@ tanh = nn.Tanh()
 relu = nn.ReLU()
 sigmoid = nn.Sigmoid()
 
-path = "/Users/prithvirajprabhu/Documents/Research projects local/CS 567 final project/Code/CS 567 final/Models/Model04_19_21_05_20_813/0.00015214361382123013.pt"
-oppath = "/Users/prithvirajprabhu/Documents/Research projects local/CS 567 final project/Code/CS 567 " \
-       "final/Outputs/v2.csv"
+def to_abs_path(relative_path):
+    script_location = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(script_location, relative_path)
+
+path = to_abs_path("Models/Model04_19_21_05_20_813/0.00015214361382123013.pt")
+oppath = to_abs_path("Outputs/v2.csv")
 layersizes = [364, 200, 125, 50, 15, 3]
 acts = [nn.Linear, relu, relu, relu, relu, softmax]
-dataset_location = "/Users/prithvirajprabhu/Documents/Research projects local/CS 567 final project/Code/CS 567 " \
-             "final/Dataset/RPMED-31-66-200_Test_values.csv"
-original_test_location = "/Users/prithvirajprabhu/Documents/Research projects local/CS 567 final project/Code/CS 567 " \
-             "final/Dataset/Richters_Predictor_Modeling_Earthquake_Damage_-_Test_Values.csv"
+dataset_location = to_abs_path("Dataset/RPMED-31-66-200_Test_values.csv")
+original_test_location = to_abs_path("Dataset/Richters_Predictor_Modeling_Earthquake_Damage_-_Test_Values.csv")
 
 def get_dimensions(my_list):
     if isinstance(my_list, list):
@@ -38,35 +39,41 @@ def get_dimensions(my_list):
 
 
 def single_pass():
-	dataset = CustomDataset(dataset_location)
-	dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+    dataset = CustomDataset(dataset_location)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
 
-	net = Net(layersizes,acts)
-	net.load_state_dict(torch.load(path))
-	net.eval()
+    net = Net(layersizes,acts)
+    net.load_state_dict(torch.load(path))
+    net.eval()
 
-	with open(original_test_location, 'r') as f:
-		reader = csv.reader(f)
-		building_ids = list(reader)[1:]
-		building_ids = np.array([building_ids[_][0] for _ in range(len(building_ids))], dtype=np.int64)
+    net_state_dict = net.state_dict()
+    for layer_name, layer_weights in net_state_dict.items():
+        # pdb.set_trace()
+        print(f"{layer_name}: weights={layer_weights}")
 
-	outputs = []
-	counter = 0
+    pdb.set_trace()
 
-	for _, x in enumerate(dataloader):
-		# pdb.set_trace()
-		output = net(x).cpu().detach().numpy() # output is a length-3 vector of
-		# probabilities that sum to 1.
-		if counter < 20:
-			counter += 1
-			print(output)
-		# pdb.set_trace()
-		outputs.extend([int(np.argmax(output))+1])
+    with open(original_test_location, 'r') as f:
+        reader = csv.reader(f)
+        building_ids = list(reader)[1:]
+        building_ids = np.array([building_ids[_][0] for _ in range(len(building_ids))], dtype=np.int64)
 
-	# outputslist = np.vstack((np.array(['building_id', 'damage_grade']), np.hstack((np.array(building_ids).reshape(-1,1), np.array(outputs).reshape(-1,1)))))
-	outputslist = np.hstack((building_ids.reshape(-1, 1), np.array(outputs).reshape(-1, 1))).tolist()
+    outputs = []
+    counter = 0
 
-	return outputslist
+    for _, x in enumerate(dataloader):
+        output = net(x).cpu().detach().numpy() # output is a length-3 vector of
+        # probabilities that sum to 1.
+        if counter < 20:
+            counter += 1
+            print(output)
+        # pdb.set_trace()
+        outputs.extend([int(np.argmax(output))+1])
+
+    # outputslist = np.vstack((np.array(['building_id', 'damage_grade']), np.hstack((np.array(building_ids).reshape(-1,1), np.array(outputs).reshape(-1,1)))))
+    outputslist = np.hstack((building_ids.reshape(-1, 1), np.array(outputs).reshape(-1, 1))).tolist()
+
+    return outputslist
 
 outputlist = single_pass()
 
